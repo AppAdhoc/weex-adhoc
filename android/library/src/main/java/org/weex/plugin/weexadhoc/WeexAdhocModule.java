7dddfe20b566dd766ef1b03e779b24f6001338f4
jiaozhengxiang@foxmail.com <jiaozhengxiang@foxmail.com>
2018-03-05 16:59:35 +0800

    android first commit

--- android/library/src/main/java/org/weex/plugin/weexadhoc/WeexAdhocModule.java
+++ android/library/src/main/java/org/weex/plugin/weexadhoc/WeexAdhocModule.java
@@ -1,5 +1,8 @@
 package org.weex.plugin.weexadhoc;
 
+import com.adhoc.adhocsdk.AdhocTracker;
+import com.adhoc.adhocsdk.ExperimentFlags;
+import com.adhoc.adhocsdk.OnAdHocReceivedData;
 import com.alibaba.weex.plugin.annotation.WeexModule;
 import com.taobao.weex.annotation.JSMethod;
 import com.taobao.weex.bridge.JSCallback;
@@ -7,7 +10,7 @@ import com.taobao.weex.common.WXModule;
 
 @WeexModule(name = "weexAdhoc")
 public class WeexAdhocModule extends WXModule {
-    
+
     //sync ret example
     //TODO: Auto-generated method example
     @JSMethod
@@ -20,5 +23,36 @@ public class WeexAdhocModule extends WXModule {
     @JSMethod
     public void asyncRet(String param, JSCallback callback) {
         callback.invoke(param);
+    }
+
+    @JSMethod
+    public <V> void adhoc_getFlag(String key, V defaultValue, JSCallback callBack) {
+        V value = AdhocTracker.getFlag(key, defaultValue);
+        callBack.invoke(value);
+    }
+
+    @JSMethod
+    public void adhoc_track(String key, Number number) {
+        AdhocTracker.track(key, number);
+    }
+
+    @JSMethod
+    public <V> void asyncGetFlag(final String key, final V defaultValue, final JSCallback callback) {
+        AdhocTracker.asyncGetFlag(new OnAdHocReceivedData() {
+            @Override
+            public void onReceivedData(ExperimentFlags experimentFlags) {
+                callback.invoke(experimentFlags.getFlag(key, defaultValue));
+            }
+        });
+    }
+
+    @JSMethod
+    public void getClientId(JSCallback callback) {
+        callback.invoke(AdhocTracker.getClientId());
+    }
+
+    @JSMethod
+    public void getCurrentExperiments(JSCallback callback) {
+        callback.invoke(AdhocTracker.getCurrentExperiments().toString());
     }
 }
