ac50678fa2c6ff25bc8ca4c4a3eeb28dd3726d1c
jiaozhengxiang@foxmail.com <jiaozhengxiang@foxmail.com>
2018-03-06 16:00:24 +0800

    change doc

--- README.md
+++ README.md
@@ -46,7 +46,7 @@ weex-adhoc是一个weex插件，可以通过weexpack快速集成，可以丰富w
 - 手动集成
   在相应工程的build.gradle文件的dependencies中添加
   ```
-  compile '${groupId}:weexadhoc:{$version}'
+  compile 'com.adhoc.weex:library:1.0.0'
   ``` 
   注意：您需要自行指定插件的groupId和version并将构建产物发布到相应的依赖管理仓库内去（例如maven）, 您也可以对插件的name进行自定义，默认将使用插件工程的名称作为name
 
--- android/library/src/main/java/org/weex/plugin/weexadhoc/WeexAdhocModule.java
+++ android/library/src/main/java/org/weex/plugin/weexadhoc/WeexAdhocModule.java
@@ -1,5 +1,7 @@
 package org.weex.plugin.weexadhoc;
 
+import android.text.TextUtils;
+
 import com.adhoc.adhocsdk.AdhocTracker;
 import com.adhoc.adhocsdk.ExperimentFlags;
 import com.adhoc.adhocsdk.OnAdHocReceivedData;
@@ -8,7 +10,11 @@ import com.taobao.weex.annotation.JSMethod;
 import com.taobao.weex.bridge.JSCallback;
 import com.taobao.weex.common.WXModule;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
+import java.util.Iterator;
 
 @WeexModule(name = "weexAdhoc")
 public class WeexAdhocModule extends WXModule {
@@ -25,13 +31,31 @@ public class WeexAdhocModule extends WXModule {
     }
 
     @JSMethod
-    public void trackWithAttribute(String key, Number number, HashMap<String, String> map) {
-        AdhocTracker.track(key, number, map);
+    public void trackWithAttribute(String key, Number number, String json) {
+        if (TextUtils.isEmpty(json))
+            return;
+
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            HashMap<String, String> hashMap = new HashMap();
+
+            Iterator<String> iterable = jsonObject.keys();
+            while (iterable.hasNext()) {
+                String jsonKey = iterable.next();
+                String jsonValue = jsonObject.optString(jsonKey);
+                hashMap.put(jsonKey, jsonValue);
+            }
+
+            AdhocTracker.track(key, number, hashMap);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @JSMethod
     public <V> void asynchronousGetFlag(final String key, final V defaultValue, int timeoutMillseconds, final JSCallback callback) {
-        AdhocTracker.asyncGetFlag(new OnAdHocReceivedData() {
+        AdhocTracker.asyncGetFlag(timeoutMillseconds, new OnAdHocReceivedData() {
             @Override
             public void onReceivedData(ExperimentFlags experimentFlags) {
                 callback.invoke(experimentFlags.getFlag(key, defaultValue));
